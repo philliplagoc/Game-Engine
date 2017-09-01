2e3491c3c3a9952466201916a8f60300e10ef614
philliplagoc <philliplagoc@gmail.com>
2017-08-31 23:51:49 -0700

    Created UI abstract class

---
+++ GameEngine 2.0/src/com/lagocp/gameEngine/ui/UI.java
@@ -0,0 +1,12 @@
+package com.lagocp.gameEngine.ui;
+
+import javafx.scene.layout.Pane;
+
+/**
+ * This abstract class builds the necessary UI components for a game.
+ * @author Phillip
+ *
+ */
+public abstract class UI extends Pane {
+
+}
