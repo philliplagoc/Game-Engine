d29906fe5b9e9e2ffa5e1daf7f6a6c7f688ca48d
philliplagoc <philliplagoc@gmail.com>
2017-09-01 10:21:22 -0700

    Added abstract methods to UI class

--- GameEngine 2.0/src/com/lagocp/gameEngine/ui/UI.java
+++ GameEngine 2.0/src/com/lagocp/gameEngine/ui/UI.java
@@ -1,12 +1,36 @@
 package com.lagocp.gameEngine.ui;
 
+import com.lagocp.gameEngine.sprite.Sprite;
+
+import javafx.scene.canvas.Canvas;
 import javafx.scene.layout.Pane;
 
 /**
  * This abstract class builds the necessary UI components for a game.
+ * 
  * @author Phillip
  *
  */
 public abstract class UI extends Pane {
+	private Canvas canvas;
 
+	public UI(Canvas canvas) {
+		this.canvas = canvas;
+	}
+
+	/**
+	 * Creates the UI for the game.
+	 */
+	public abstract void create();
+
+	/**
+	 * Places the canvas.
+	 * The canvas is where the Sprites and such are drawn onto.
+	 */
+	public abstract void placeCanvas();
+	
+	/**
+	 * Updates the UI depending on the Sprites passed in.
+	 */
+	public abstract void updateUI(Sprite... sprites);
 }
